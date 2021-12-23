8861c532071330dafa24f6d4a27fde2b83c9f733
Nicolás Órdenes <nicordenes@gmail.com>
2021-12-23 15:42:39 -0300

    Add display object

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+// display object will hold input information
+// and update screen readings
+theDisplay = {
+    firstNumber: "0",
+    secondNumber: "",
+    operation: "",
+    screen: "",
+    update() {
+        this.screen = "";
+        if (this.firstNumber) this.screen += `${this.firstNumber}`;
+        if (this.operation) this.screen += ` ${this.operation}`;
+        if (this.secondNumber) this.screen += ` ${this.secondNumber}`;
+        return this.screen
+    }
+}
+
 // basic math functions
 const add = (a, b) => a + b
 const subtract = (a, b) => a - b
