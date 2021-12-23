391fb56ad25f13e59f63aa5baad9cb96bbecfe11
Nicolás Órdenes <nicordenes@gmail.com>
2021-12-23 16:25:30 -0300

    Update function location on file

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+// event listeners
+buttons = document.querySelectorAll('.button')
+buttons.forEach(
+    button => button.addEventListener('click', e => {
+        theDisplay.newInput(e.target.textContent)
+    })
+);
+
+// basic math functions
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+
 // display object will hold input information
 // and update screen readings
 theDisplay = {
@@ -43,16 +57,4 @@ theDisplay = {
     },
 }
 
-// basic math functions
-const add = (a, b) => a + b
-const subtract = (a, b) => a - b
-const multiply = (a, b) => a * b
-const divide = (a, b) => a / b
-
-// numeric buttons event listeners
-buttons = document.querySelectorAll('.button.number')
-buttons.forEach(
-    button => button.addEventListener('click', e => {
-        theDisplay.newInput(e.target.textContent)
-    })
-);
+theDisplay.updateScreen()
