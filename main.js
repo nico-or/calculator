cd9273738cc406119924631a1dedd58965cec19a
Nicolás Órdenes <nicordenes@gmail.com>
2021-12-23 15:20:34 -0300

    Add numeric buttons event listeners

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ const subtract = (a, b) => a - b
 const multiply = (a, b) => a * b
 const divide = (a, b) => a / b
 
+// numeric buttons event listeners
+buttons = document.querySelectorAll('.button.number')
+buttons.forEach(
+    button => button.addEventListener('click', e => {
+        console.log(e.target.textContent)
+    })
+);
